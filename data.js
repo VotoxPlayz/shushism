08275056031037ac68a9300580436b515dd273fa
VotoxPlayz <126447930+VotoxPlayz@users.noreply.github.com>
2025-10-27 17:58:59 +1100

    Update data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const BOOK_DATA = [
         pages: [
             // Page 1:1
             `
-                <p class="font-semibold text-xl text-indigo-400">Page 1</p>
+                <p class="font-semibold text-xl text-indigo-400">Pilot: Part I</p>
                 <p>1 - At the start of the universe Shush created the word "shush" and the colour "yellow".</p>
                 <p>2 - Shush then created space, making stars and galaxies</p>
                 <p>3 - The first day, shush made the death of stars called "supernova".</p>
@@ -27,7 +27,7 @@ const BOOK_DATA = [
         pages: [
             // Page 2:1
             `
-                <p class="font-semibold text-xl text-indigo-400">Page 2</p>
+                <p class="font-semibold text-xl text-indigo-400">Creation: Part I</p>
                 <p>1 - In the beginning of Earth, Shush created the light and darkness, the first minute.</p>
                 <p>2 - Shush then created the sealife and landlife, creating the seas and an island called "Pangaea", the second minute.</p>
                 <p>3- Shush made 200 million living people, each with lives and a purpose, the third minute.</p>
@@ -50,7 +50,7 @@ const BOOK_DATA = [
         pages: [
             // Page 2:1
             `
-                <p class="font-semibold text-xl text-indigo-400">Page 3</p>
+                <p class="font-semibold text-xl text-indigo-400">Smells: Part I</p>
                 <p>1- Shush saw that the Pajeetistinians were being too hated and needed to something about it.</p>
                 <p>2- Shush merged Pajeetistan and Asia together, making 2 races in 1 continent</p>
                 <p>3- Shush saw that the Pajeetistinians were saying they were from Asia.</p>
@@ -62,6 +62,131 @@ const BOOK_DATA = [
                 <p>9- Shush then wiped all the faces of Earth on Earth and caused another big bang, this time he did something different.</p>
             `,
             // Add Page 2:2, 2:3, etc. here
+        ]
+    },
+
+    {
+        title: "Evolution",
+        pages: [
+            // Page 2:1
+            `
+                <p class="font-semibold text-xl text-indigo-400">Evolution: Part I</p>
+                <p>1- Shush saw the universe rapidly expanding and saw that it was good.</p>
+                <p>2- Shush then created only Earth and Sun.</p>
+                <p>3- Shush made Sun hot and Earth cool.</p>
+                  <p>4- Shush went into Earth and created a small form of life called "Protocells"</p>
+                    <p>5- Shush made the protocells small and hard to see.</p>
+                      <p>6- Shush then only created the land and the sea</p>
+                        <p>7- Shush made a little island on top of the sea for him to watch the Protocells grow.</p>
+                          <p>8- Shush waited and waited and waited but nothing happened to the cells.</p>
+                            <p>9- After 750 Million years of waiting, shush noticed that the protocells had evolved into a multicellular life.</p>
+                              <p>10- Shush noticed that the cell had multiple cells.</p>
+            `,
+              `
+                <p class="font-semibold text-xl text-indigo-400">Evolution: Part II</p>
+                <p>11- Shush then waited another 750 Million years, being sure to duplicate the cells.</p>
+                <p>12- After another 750 Million years, the cells turned into small tiny "animals".</p>
+                <p>13- Shush waited for the animals to evolve, another 750 million years.</p>
+                <p>14- Shush saw that the animals turned into Fish and Tetrapods.</p>
+                <p>15- Shush had to make a box of water to keep the fish from dying.</p>
+                <p>16- Shush waited another 750 million years for the fish and tetrapods to turn into anything.</p>
+                <p>17- Shush noticed that the fish turned into Monkeys and the tetrapods turned into great apes.</p>
+                <p>18- Shush waited another 750 million years for the monkeys and great apes to transform.</p>
+                <p>19- Shush then saw that all the life on his little island had turned into "Homo sapiens".</p>
+                <p>20- Shush noticed that each of the Homosapiens had thoughts and were making sounds to eachother.</p>
+               
+            `,
+            
+        ]
+    },
+
+     {
+        title: "Language",
+        pages: [
+            // Page 2:1
+            `
+                <p class="font-semibold text-xl text-indigo-400">Language: Part I</p>
+                <p>1- Shush distributed the homosapiens across 4 Islands based on skin colour. White, Yellow, Brown and Black.</p>
+                <p>2- Shush noticed that the brownies were once again being treated unfairly.</p>
+                <p>3- No one shared any grass they found with the brownies.</p>
+                <p>4- Shush noticed that the only words the homosapiens were saying was "shush".</p>
+                <p>5- Shush then invented food, a way for humans to eat and feel good.</p>
+                <p>6- Shush invented fruits, he called the red one Apple, he called the green one Pear, he called the orange one Orange, he called the orange one Mandarin, he called the purple one Blueberry.</p>
+                <p>7- Shush then invented language, a way for the humans to speak to eachother.</p>
+                <p>8- Shush called the language "Mandarin" and based it off of the orange food.</p>
+                <p>9- The first words shush invented in "Mandarin" was "Ni hao" meaning hello.</p>
+            
+            `,
+              `
+                <p class="font-semibold text-xl text-indigo-400">Language: Part II</p>
+                <p>10- Shush made other basic words like "wo" which means I, "ni" which means you and "Zaijian" meaning bye.</p>
+                <p>11- Shush then made a word called "Hēi guǐ" it was a word that had the purpose of being used against the black skinned humans.</p>
+                <p>12- Shush then made another word called "Gālí jǔjué zhě", it was a word that had the purpose of being used against the brown skinned humans.</p>
+                <p>13- Shush then made another word called "Kòngbái huàbù", it was a word that had the purpose of being used against the white skinned humans.</p>
+                <p>14- Shush was biased and made no word that had the purpose of being used against the yellow skinned humans.</p>
+                <p>15- Shush invented pots and pans, used to store food.</p>
+                <p>16- Shush noticed that when he banged the pans together it would make a sound "ching ching ching".</p>
+                <p>17- Shush noticed that when he banged the pots together, it would make a souns "chong chong chong".</p>
+                <p>18- Shush used these sounds against the yellow skinned, only if they were misbehaving.</p>
+                <p>19- Shush invented another word called "Ānjìng de", it was a way to tell all humans to be quiet.</p>
+               
+               
+            `,
+            
+        ]
+    },
+
+    {
+        title: "Reproduce",
+        pages: [
+            // Page 2:1
+            `
+                <p class="font-semibold text-xl text-indigo-400">Reproduce: Part I</p>
+                <p>1- Shush then invented pieces of land called "Continents" these pieces of land would have more land in them called "Countries" these countries would have more land in them called "Cities" these cities would have more land in them called "Suburbs" these suburbs would have more land in them called "Streets" these streets would have more land in them called "Houses".</p>
+                <p>2- Shush then removed cities, suburbs, streets and houses.</p>
+                <p>3- Shush then divided the humans again into 6 Continents called 1. Gunland, 2. Whiteland, 3. Yellowland, 4. South Gunland, 5. Africanistan and finally, 6. Oceaneseiah.</p>
+                <p>4- Shush divided the whites into Gunland, Whiteland and South Gunland.</p>
+                <p>5- Shush divided the yellows into Yellowland.</p>
+                <p>6- Shush divided the browns into Yellowland aswell; this caused problems later.</p>
+                <p>7- Shush divided the blacks into Africanistan.</p>
+                <p>8- Shush then gave all humans except the Africanistans the ability to reproduce with strict rules.</p>
+                <p>9- Yellows and whites could freely reproduce together.</p>
+                <p>10- Browns could only reproduce with browns.</p>
+             
+            
+            `,
+              `
+                <p class="font-semibold text-xl text-indigo-400">Reproduce: Part II</p>
+                <p>11- Blacks did not have the ability to reproduce.</p>
+                <p>12- Shush then split up 51% of the humans into a category called "Male".</p>
+                <p>13- Shush then split up 49% of the humans into a category called "Womale".</p>
+                <p>14- Shush gave the males weenies which were long sticks of skin.</p>
+                <p>15- Shush gave the womales pusses which were 2 strips with 2 holes.</p>
+                <p>16- Shush made the 2nd hole be able to expand, allowing male weenies to enter it.</p>
+                <p>17- All the humans on Earth noticed that they were naked and everybody grabbed pieces of grass to cover themselves</p>
+                <p>18- The blacks were bullied because they did not have any grass, only dirt.</p>
+                <p>19- Shush gave the womales titties which were 2 squishable balls that allowed them to have milk and they attracted males.</p>
+                <p>20- All the womales on Earth covered their titties with grass, except for the blacks, they could not afford grass.</p>
+                <p>21- Shush gave the yellows small titties and the whites bigger.</p>
+             
+               
+               
+            `,
+              `
+                <p class="font-semibold text-xl text-indigo-400">Reproduce: Part III</p>
+                <p>22- Mankind was beginning to reproduce, and the population of only 14 Was beginning to overpopulate</p>
+                <p>23- The 2 people on Gunland "Olivia and Liam" reproduced, creating oliam.</p>
+                <p>24- The 2 people on Whiteland "Felix and elodie" reproduced, creating Felodix.</p>
+                <p>25- The 2 people on Yellowland "Ling Ling and Wang" reproduced, creating Lingwang.</p>
+                <p>26- The 2 brown people on Yellowland "Pajeet and Aapkeseho" reproduced, creating pajkeseet.</p>
+                <p>27- The 2 people on South Gunland "Camilia and Fernando" reproduced, creating camnandilia.</p>
+                <p>28- The 2 People on Oceanesiah "Charlotte and Oliver" reproduced, creating charliver.</p>
+                <p>29- The 2 People on Africanistan "clehcluhkclohkcluh and kehkohcluhcluhk" did not reproduce.</p>
+                <p>30- Shush noticed that the africanistanians did not reproduce, and gave them the ability too.</p>
+                <p>31- Shush only allowed the blacks to reproduce with blacks and browns, but browns could not reproduce with blacks.</p>
+                <p>32- The 2 People on Africanistan "clehcluhkclohkcluh and kehkohcluhcluhk" then reproduced, creating oogadoogadoo.</p>
+            `,
+            
         ]
     }
    
