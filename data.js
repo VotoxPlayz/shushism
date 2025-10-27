9be1e4300e254a8c63c8b2532234e98c5c860a8a
VotoxPlayz <126447930+VotoxPlayz@users.noreply.github.com>
2025-10-27 17:45:56 +1100

    Update data.js

--- data.js
+++ data.js
@@ -9,13 +9,13 @@ const BOOK_DATA = [
                 <p class="font-semibold text-xl text-indigo-400">Page 1</p>
                 <p>1 - At the start of the universe Shush created the word "shush" and the colour "yellow".</p>
                 <p>2 - Shush then created space, making stars and galaxies</p>
-                <p>3- The first day, shush made the death of stars called "supernova".</p>
-                <p>4- 13.8 Billion years later, Shush destroyed the universe in the "big bang"</p>
-                <p>5- The big bang caused a new universe to grow rapidly.</p>
-                <p>6- Shush saw that the universe expanding was good, he then created the solar system.</p>
-                <p>7- He created 11 planets called, "Sun, Mercury, Penus, Earth, Moon, Mars, Saturn, Youranus, Pluto and Eris".</p>
-                <p>8- Shush made Sun hot and Moon cold.</p>
-                <p>9- Shush then made earth habitable.</p>
+                <p>3 - The first day, shush made the death of stars called "supernova".</p>
+                <p>4 - 13.8 Billion years later, Shush destroyed the universe in the "big bang"</p>
+                <p>5 - The big bang caused a new universe to grow rapidly.</p>
+                <p>6 - Shush saw that the universe expanding was good, he then created the solar system.</p>
+                <p>7 - He created 11 planets called, "Sun, Mercury, Penus, Earth, Moon, Mars, Saturn, Youranus, Pluto and Eris".</p>
+                <p>8 - Shush made Sun hot and Moon cold.</p>
+                <p>9 - Shush then made earth habitable.</p>
                 <p>10 - Shush made a shiny object called 'The Emblem Of Shush'</p>
                 <p>11 - Shush took out his emblem of Shush from his pocket, which he gave to everything that he created. Even the planet Earth had the emblem of Shush hidden inside it.</p>
             `,
@@ -27,11 +27,42 @@ const BOOK_DATA = [
         pages: [
             // Page 2:1
             `
-                <p class="font-semibold text-xl text-indigo-400">Chapter 2: Silent Orbit Begins</p>
-                <p>A new chapter means a fresh start, a clean screen, and a deeper dive into the world of Shushism. The pressure lock hissed, a sound that in this new context felt deafeningly loud, a violation of the quiet they had all sworn to uphold...</p>
+                <p class="font-semibold text-xl text-indigo-400">Page 2</p>
+                <p>1 - In the beginning of Earth, Shush created the light and darkness, the first minute.</p>
+                <p>2 - Shush then created the sealife and landlife, creating the seas and an island called "Pangaea", the second minute.</p>
+                <p>3- Shush made 200 million living people, each with lives and a purpose, the third minute.</p>
+                <p>4- Shush invents time, starting with the biggest term as a "year" and calls it 365 Days, the world started at 300M BC</p>
+                <p>5- shush saw that the humans were all wrecking havoc on Pangaea, every million years the island was slowly drifting apart.</p>
+                <p>6- After 300 Million years, humanity had multiplied but Pangaea was no more.</p>
+                <p>7- Shush split up the islands into 8 Parts, 1. Gunland, 2. Whiteland, 3. Asia, 4. South Gunland, 5. Monkeypoxvill, 6. Oceanie, 7. Coldland and 8. Pajeetistan</p>
+                 <p>8- Shush made the Pjaeetistinians brown, to show that they should be excluded.</p>
+                <p>9- Shush made the monkeypoxvillians black, to show that they should be feared.</p>
+                <p>10- Shush made the Asians yellow, to show that they were normal.</p>
+                <p>11- Shush made the Gunlandians, Oceanians and Whitelandians White, showing that they ruled over countries.</p>
+                <p>12- Shush made Coldland too cold for habitation, showing that no one shall live there.</p>
+            `,
+            // Add Page 2:2, 2:3, etc. here
+        ]
+    },
+
+     {
+        title: "Smells",
+        pages: [
+            // Page 2:1
+            `
+                <p class="font-semibold text-xl text-indigo-400">Page 3</p>
+                <p>1- Shush saw that the Pajeetistinians were being too hated and needed to something about it.</p>
+                <p>2- Shush merged Pajeetistan and Asia together, making 2 races in 1 continent</p>
+                <p>3- Shush saw that the Pajeetistinians were saying they were from Asia.</p>
+                <p>4- Shush cursed the Pajeetistinians, making them smelly, poor and have a red dot.</p>
+                <p>5- Shush made the Pajeetistinians use a different currency from the rest of the world called "rupees".</p>
+                <p>6- Rupees weren't worth much compared to normal currency but the Pajeetistinians were still acting up..</p>
+                <p>7- Shush then killed every brownie, eliminating them from earth.</p>
+                 <p>8- The end of the world was near.</p>
+                <p>9- Shush then wiped all the faces of Earth on Earth and caused another big bang, this time he did something different.</p>
             `,
             // Add Page 2:2, 2:3, etc. here
         ]
     }
-    // Add more chapters here
+   
 ];
